aecb1541f398e975e326f608c223ada8a0f591fd
RoffeRuff42 <anderssonrolf94@gmail.com>
2025-12-01 13:53:00 +0100

    updated Course and Grades

--- CourseAdministrationSystem/Models/Course.cs
+++ CourseAdministrationSystem/Models/Course.cs
@@ -1,8 +1,26 @@
 ﻿using System;
 
-public class Class1
+public class Course
 {
-	public Class1()
-	{
-	}
+	[Key]
+	public int CourseId { get; set; }
+
+	[Required]
+	[StringLength(100)]
+	public string CourseName { get; set; }
+	[Required]
+    public DateTime CourseStart { get; set; }
+	[Required]
+	public DateTime CourseEnd { get; set; }
+
+    //Foreign key to Classroom
+    public int ClassroomId { get; set; }
+	[ForeignKey("FkClassroomId")]
+	public Classroom Classroom { get; set; }
+
+    //Foreign key to Teacher
+	public int TeacherId { get; set; }
+	[ForeignKey("FkTeacherId")]
+	public Teacher Teacher { get; set; }
+
 }
--- CourseAdministrationSystem/Models/Grades.cs
+++ CourseAdministrationSystem/Models/Grades.cs
@@ -1,8 +1,30 @@
 ﻿using System;
 
-public class Class1
+public class Grades
 {
-	public Class1()
-	{
-	}
+    [Key]
+    public int GradeId { get; set; }
+
+    [Required]
+    public int Grade { get; set; }
+
+    [Required]
+    public DateTime GradesDate { get; set; }
+
+    //Foreign key to Student
+    public int StudentId { get; set; }
+    [ForeignKey("FkStudentId")]
+    public Student Student { get; set; }
+
+    //Foreign key to Course
+    public int CourseId { get; set; }
+    [ForeignKey("FkCourseId")]
+    public Course Course { get; set; }
+
+    //Foreign key to Teacher
+    public int TeacherId { get; set; }
+    [ForeignKey("FkTeacherId")]
+    public Teacher Teacher { get; set; }
+
+
 }

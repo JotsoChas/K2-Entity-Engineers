9e4787d91ecdf2f33b202c2528938675b1ee4c43
JocoChas <joco.borghol@chasacademy.se>
2025-12-01 10:37:54 +0100

    Added EF Core setup and scaffolding for initial database tables

---
+++ CourseAdministrationSystem/data/K2DbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using CourseAdministrationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseAdministrationSystem.Data;
+
+public partial class K2DbContext : DbContext
+{
+    public K2DbContext()
+    {
+    }
+
+    public K2DbContext(DbContextOptions<K2DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Classroom> Classrooms { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=K2DB;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Classroom>(entity =>
+        {
+            entity.HasKey(e => e.ClassroomId).HasName("PK__Classroo__11618EAA4D9EFCBC");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Student__32C52B9985918422");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

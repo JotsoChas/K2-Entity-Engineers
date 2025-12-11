cbfc0655c375c4c6a5a1d0fd2042a2b72be7dbe0
JocoChas <joco.borghol@chasacademy.se>
2025-12-11 10:24:35 +0100

    changed JOIN to INNER JOIN

--- CourseAdministrationSystem/Docs/Views/v_StudentCourseGradeTeacher.sql
+++ CourseAdministrationSystem/Docs/Views/v_StudentCourseGradeTeacher.sql
@@ -1,25 +1,23 @@
 ﻿CREATE OR ALTER VIEW View_StudentCourseGradeTeacher AS
+
 SELECT
-    s.StudentId,
-    s.StudentFirstName,
-    s.StudentLastName,
-
-    c.CourseId,
-    c.CourseName,
-
-    g.Grade,
-    g.GradesDate,
-
-    t.TeacherId,
-    t.TeacherFirstName,
-    t.TeacherLastName
+s.StudentId,
+s.StudentFirstName,
+s.StudentLastName,
+c.CourseId,
+c.CourseName,
+g.Grade,
+g.GradesDate,
+t.TeacherId,
+t.TeacherFirstName,
+t.TeacherLastName
 FROM Student s
-LEFT JOIN StudentCourses sc 
-    ON s.StudentId = sc.StudentId
-LEFT JOIN Courses c 
-    ON sc.CourseId = c.CourseId
-LEFT JOIN Grades g 
-    ON g.StudentId = s.StudentId 
-    AND g.CourseId = c.CourseId
-LEFT JOIN Teacher t 
-    ON g.TeacherId = t.TeacherId;
+LEFT JOIN StudentCourses sc
+ON sc.StudentId = s.StudentId
+LEFT JOIN Courses c
+ON sc.CourseId = c.CourseId
+LEFT JOIN Grades g
+ON g.StudentId = s.StudentId
+AND g.CourseId = c.CourseId
+LEFT JOIN Teacher t
+ON t.TeacherId = c.TeacherId;
--- CourseAdministrationSystem/Docs/Views/v_StudentCourses.sql
+++ CourseAdministrationSystem/Docs/Views/v_StudentCourses.sql
@@ -1,9 +1,9 @@
 ﻿CREATE OR ALTER VIEW View_StudentCourses AS
 SELECT 
-    s.StudentId,
-    s.StudentFirstName,
-    s.StudentLastName,
-    c.CourseName
+s.StudentId,
+s.StudentFirstName,
+s.StudentLastName,
+c.CourseName
 FROM Student s
 LEFT JOIN StudentCourses sc ON s.StudentId = sc.StudentId
 LEFT JOIN Courses c ON sc.CourseId = c.CourseId;
